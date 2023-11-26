46deaf3a944aef441c171de9578186c0c7d62ccc
yvam-augusto <yvam_augusto@hotmail.com>
2023-11-25 23:21:09 -0300

    feat/Criação do componente Card.

--- app_frontend/lib/components/card_component.dart
+++ app_frontend/lib/components/card_component.dart
@@ -7,21 +7,56 @@ class CardComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    String nomeMusica = musica!.nome;
+    if (nomeMusica.length > 20) {
+      nomeMusica = '${nomeMusica.substring(0, 30)}...';
+    }
+
     return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        Text(musica!.nome,
-            style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-                fontWeight: FontWeight.bold)),
-        const SizedBox(height: 5),
-        Text(musica!.album,
-            style: const TextStyle(color: Colors.white, fontSize: 15)),
-        const SizedBox(height: 5),
-        Text(musica!.autor,
-            style: const TextStyle(color: Colors.white, fontSize: 15))
-      ]),
+      padding: const EdgeInsets.only(left: 10, right: 10, top: 6, bottom: 6),
+      child: GestureDetector(
+        onTap: () {
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 34, 34, 34),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          padding: const EdgeInsets.only(left: 16, right: 16, top: 12, bottom: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                nomeMusica,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 5),
+              Row(
+                children: [
+                  Text(
+                    musica!.album + '  |  ',
+                    style: const TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  Text(
+                    musica!.autor + '  |  ',
+                    style: const TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                  Text(
+                    musica!.duracao,
+                    style: const TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
